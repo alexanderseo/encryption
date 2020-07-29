57786680c5c82eb23df2d680943edf08abd6e4cd
alexanderseo <alexanderzanin@yandex.ru>
2020-07-29 23:15:28 +0300

    добавлена валидация атрибута класса

--- bubblesort/arraybub.py
+++ bubblesort/arraybub.py
@@ -3,6 +3,17 @@
 class ArrayBub:
     def __init__(self, size):
         self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected a INT')
+        self._size = value
+
     def getarraybub(self):
         arr1 = [x for x in range(0, self.size, 3)]
         arr2 = [x for x in range(self.size, self.size + 300, 4)]
